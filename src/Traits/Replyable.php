39da4bb1dcfdc1fd213c521d879f74221543598a
ulasgokce <ulasgokce98@gmail.com>
2023-05-30 00:13:09 +0300

    hotfix

--- src/Traits/Replyable.php
+++ src/Traits/Replyable.php
@@ -330,7 +330,7 @@ trait Replyable
 		if ($threadId) {
 			$this->setHeader('In-Reply-To', $this->getMessageIdHeader());
 			$this->setHeader('References', $this->getHeader('References'));
-			$this->setHeader('Message-ID', $this->getMessageIdHeader());
+			// $this->setHeader('Message-ID', $this->getMessageIdHeader());
 		}
 	}
 
